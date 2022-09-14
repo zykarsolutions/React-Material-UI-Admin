cd4e226f0492ef1dbd16a7d23ce3509999a336d5
Moses Mwila <moseshb17@gmail.com>
2022-09-14 12:23:42 +0200

    Added New User & Product Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
+import { productInputs, userInputs } from "./formSource"
+
 import {
   BrowserRouter,
   Routes,
@@ -21,12 +23,12 @@ function App() {
       <Route path="users">
         <Route index element={<List/>}/>
         <Route path=":userId" element={<Single/>}/>
-        <Route path="new" element={<New/>}/>
+        <Route path="new" element={<New inputs={userInputs} title="Add New User"/>}/>
       </Route>
       <Route path="products">
         <Route index element={<List/>}/>
         <Route path=":productId" element={<Single/>}/>
-        <Route path="new" element={<New/>}/>
+        <Route path="new" element={<New inputs={productInputs} title="Add New Product"/>}/>
       </Route>
       </Route>
     </Routes>
